9339903b0e577ef6e7859a75a703db515308adaa
khoirurrozikin86 <khoirurrozikin86@baliproject.id>
2025-06-27 10:30:40 +0700

    up

--- app/(main)/[...missing]/page.tsx
+++ app/(main)/[...missing]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
 
 export default function CatchAllNotFound() {
-    notFound(); // akan trigger not-found.tsx dari layout inii
+    notFound(); // akan trigger not-found.tsx dari layout iniis
 }
